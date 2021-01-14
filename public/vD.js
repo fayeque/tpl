995aee06e2d522b37da577538ef7d805829e3432
Fayeque Hannan <fayeque@example.com>
2021-01-14 13:13:51 +0530

    bsxhghxg

---
+++ public/vD.js
@@ -0,0 +1,39 @@
+// let vt=document.querySelector(".vv");
+
+// console.log("vttt===================================",vt);
+// function viewD(event){
+//   var id=event.target.dataset.value;
+// console.log(event);
+// console.log("here")
+// console.log(id);
+// console.log(`https://htobacco.herokuapp.com/viewDetail/${id}`);
+
+// if('caches' in window){
+// console.log(`https://htobacco.herokuapp.com/viewDetail/${id}`);
+// caches.match(`https://htobacco.herokuapp.com/viewDetail/${id}`)
+// .then((response) => {
+// if(response){
+// console.log("from here",response);
+// return response.text();
+// }
+// })
+// .then((data) => {
+// // console.log("data",data);
+// if(data){
+// console.log("datatat");
+// document.body.innerHTML = data;
+// }
+
+// });
+// }
+
+// fetch(`https://htobacco.herokuapp.com/viewDetail/${id}`)
+// .then((response) => {
+// console.log("response from net",response);
+// return response.text();
+// })
+// .then((data) => {
+// // console.log("data",data);
+// document.body.innerHTML = data;
+// });
+// }
